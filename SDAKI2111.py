03b9ef77267d7bee181344dcf5df9c6a1318982b
aryafchandra <chandraryaf@gmail.com>
2022-09-10 11:22:57 +0700

    rev lab adt

--- SDAKI2111.py
+++ SDAKI2111.py
@@ -1,3 +1,40 @@
 import SDALabADT as ADT
 
-stack = ADT.PriorityQueue
+stack = ADT.Stack()
+char = input()
+S = []
+count = 1
+correct = True
+
+
+for i in char:
+    x = (i, count)
+    if stack.isEmpty():
+        stack.push(x)
+    elif i == ')' and stack.peek()[0] == '(':
+        y = stack.pop()
+        S.append((y[1],x[1]))
+    elif i == '}' and stack.peek()[0] == '{':
+        y = stack.pop()
+        S.append((y[1],x[1]))
+    elif i == ']' and stack.peek()[0] == '[':
+        y = stack.pop()
+        S.append((y[1],x[1]))
+    elif i in (')','}',']'):
+        correct = False
+        break
+    else:
+        stack.push(x)
+    count+=1
+
+if not stack.isEmpty():
+    correct = False
+
+if correct:
+    S = sorted(S)
+    print('VALID')
+    print(len(S))
+    for i in S:
+        print(str(i[0]) + " " + str(i[1]))
+else:
+    print('INVALID')
